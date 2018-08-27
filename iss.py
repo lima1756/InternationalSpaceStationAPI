fd30cbb0ff28d4464e2116c89f2dd5aaee8b583a
Iván Morett <luisivanmorett@gmail.com>
2018-08-27 15:05:24 -0500

    Obtained info

--- iss.py
+++ iss.py
@@ -1,4 +1,6 @@
 import requests ## Importamos la libreria que nos permite hacer solicitudes web
+import json ## Importamos la libreria para manejar datos en formato JSON
+from datetime import datetime
 
 ## Servidor al que hacemos la llamda
 server = "http://api.open-notify.org/"
@@ -11,7 +13,6 @@ server = "http://api.open-notify.org/"
 ## Revisamos el estado de la solicitud
 # print(response)
 
-
 ## Cuando la estacion internacional esta sobre Gdl?
 ## La documentacion nos dice que usemos otro endpoint
 endpoint = "iss-pass.json"
@@ -20,6 +21,7 @@ endpoint = "iss-pass.json"
 params = {"lat": 20.65, "lon": 103.34}
 
 response = requests.get(server+endpoint, params)
-print(response)
-
-print(response.content)
+data = response.json()
+print("---------------Estara sobre Guadalajara en: -------------")
+print(datetime.utcfromtimestamp(data['response'][0]['risetime']).strftime('%d-%m-%Y %H:%M:%S'))
+print(datetime.utcfromtimestamp(data['response'][1]['risetime']).strftime('%d-%m-%Y %H:%M:%S'))
